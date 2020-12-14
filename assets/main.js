65b35866c75003a25fc06430327bb77f32a07b05
antonio <antoniorocca46@gmail.com>
2020-12-14 20:30:01 +0100

    Final Commit

---
+++ assets/main.js
@@ -0,0 +1,10 @@
+// Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
+// Bonus: Aggiungere un'immagine presa anch'essa da un data.
+
+var app = new Vue({
+    el: "#root",
+    data: {
+      message: "Welcome Vue",
+      imgURL: "https:/vuejs.org/images/logo.png"
+    }
+})
